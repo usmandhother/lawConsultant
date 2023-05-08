e5862b74af26c0908859408ae481f4bde7c00c5c
bsb-github <codewithsheri64@gmail.com>
2023-05-08 17:02:27 +0500

    lawyer recommender

--- main.py
+++ main.py
@@ -154,6 +154,6 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0",debug=True, port=os.getenv("PORT", default=5000))
+    app.run(port=os.getenv("PORT", default=5000))
 
 # In[ ]:
